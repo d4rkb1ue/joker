8437798c22da038e3f9918f5fec6e60756eeaf59
ray0 <7874364@gmail.com>
2016-04-05 16:02:32 +0800

    really add login and register

--- app.js
+++ app.js
@@ -5,7 +5,16 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+var settings = require('./settings');
 var routes = require('./routes/index');
+
+var flash = require('connect-flash');
+
+
+
 
 var app = express();
 
@@ -14,9 +23,20 @@ app.set('port',process.env.PORT || 3000);
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(flash());
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+//
+
+app.use(session({
+	secret: settings.cookieSecret,
+	key: settings.cookieName,//cookie name
+	cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+	store: new MongoStore({
+		url: settings.mongodbUrl
+	})
+}));
+
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -26,9 +46,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 routes(app);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
 });
 
 // error handlers
@@ -36,23 +56,23 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
+	app.use(function(err, req, res, next) {
+		res.status(err.status || 500);
+		res.render('error', {
+			message: err.message,
+			error: err
+		});
+	});
 }
 
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: {}
+	});
 });
 
 app.listen(app.get('port'),function(){
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,95 @@
 var express = require('express');
-
+var crypto = require('crypto');
+var User = require('../models/user.js');
 module.exports = function(app){
 	app.get('/', function(req, res, next) {
-		res.render('index');
+		res.render('index',renderSession('众客',req));
 	});
-	app.get('/reg',function(req,res){
-		res.render('reg');
+	app.get('/register',function(req,res){
+		res.render('register', renderSession('注册 - 众客',req));
 	});
-	app.post('/reg',function(req,res){
+	app.post('/register',function(req,res){
+		var name = req.body.name;
+		var password = req.body.password;
+		var email = req.body.email;
+		//生成md5
+
+		var md5 = crypto.createHash('md5');
+		var md5_pass = md5.update(password).digest('hex');
+		var newUser = new User({
+			name: name,
+			password: md5_pass,
+			email: email,
+		});
+		User.getbyName(newUser.name, function(err,user){
+			if(err){
+				req.flash('error',err);
+				return res.redirect('/register');
+			}
+			if(user){
+				req.flash('error','用户名已存在！');
+				return res.redirect('/register');
+			}
+			User.getbyKey_Value({email:newUser.email}, function(err,user){
+				if(err){
+					req.flash('error',err);
+					return res.redirect('/register');
+				}
+				if(user){
+					req.flash('error','邮箱已注册');
+					return res.redirect('/register');
+				}
+				newUser.save(function (err,user){
+					if(err){
+						req.flash('error',err);
+						return res.redirect('/register');
+					}
+					req.session.user = newUser;
+					return res.redirect('/');
+				});
+			});
+		});
+
 	});
+
+	app.get('/login',function(req,res){
+		res.render('login',renderSession('登录 - 众客',req));
+	});
+	app.post('/login',function(req,res){
+		var md5 = crypto.createHash('md5');
+		var password = md5.update(req.body.password).digest('hex');
+		//检查用户是否存在
+
+		User.getbyNameOrEmail(req.body.nameoremail, function(err,user){
+			if(!user){
+				req.flash('error','用户名或Email没找到');
+				return res.redirect('/login');
+			}
+			if(user.password != password){
+				req.flash('error','密码错误');
+				return res.redirect('/login');
+			}
+			req.session.user = user;
+			res.redirect('/');
+		});
+	});
+
+
+
+
+	app.get('/logout',function(req,res){
+		req.session.user = null;
+		res.redirect('/login');
+	});
+
+	function renderSession(title, req){
+		return {
+			title: title,
+			user: req.session.user,
+			success: req.flash('success').toString(),
+			error: req.flash('error').toString()
+		};
+	};
+
 };
+
