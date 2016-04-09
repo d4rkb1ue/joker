bde3208acc655ec99da6dbe580caa434e9dae080
d4rkb1ue <7874364@gmail.com>
2016-04-10 02:03:29 +0800

    finish user r/w, project_funding w

--- models/project_funding.js
+++ models/project_funding.js
@@ -1,64 +1,157 @@
-var mongodb = require('./db');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = require('../settings').mongodbUrl;
+var async = require('async');
 
-function Project(name, title, post) {
-  this.title = title;
-  this.feature_image = feature_image; //url
- this.short_blurb = short_blurb;
-this.category = categroy;
-this.funding_goal = funding_goal;
-this.funding_duration = funding_duration;
-this.reward = reward; //should be [{amount: ,title: ,info: , limit: ,},..]
-this.video = video; //url
 
-this.description = description;
+function Project_funding(title, description, author_id, feature_image, short_blurb,
+                         project_category, funding_goal, funding_duration, reward, video,
+                         risk_challenges, author_name, author_photo, author_link, 
+                         author_bio, author_location, author_contact, author_email, 
+                         email_append) {
 
-this.risk_challenges = risk_challenges;
+    this.title = title;
+    this.description = description;
+    this.author_id = author_id;
 
-this.author_name = author_name;
-this.author_photo = author_photo;
-this.author_link = author_link; //should be [], include weibo, wechat, etc. can be parsed by url into icons
-this.author_bio = author_bio; //infomations
-this.author_location = author_location;
+    this.feature_image = feature_image; //url
+    this.short_blurb = short_blurb;
+    this.category = project_category;
+    this.funding_goal = funding_goal;
+    this.funding_duration = funding_duration;
+    this.reward = reward; //should be [{amount: ,title: ,info: , limit: ,},..]
+    this.video = video; //url
+    this.risk_challenges = risk_challenges;
 
-this.author_contact = author_contact;
-this.author_email = author_email;
+    this.author_name = author_name;
+    this.author_photo = author_photo;
+    this.author_link = author_link; //should be [], include weibo, wechat, etc. can be parsed by url into icons
+    this.author_bio = author_bio; //infomations
+    this.author_location = author_location;
+
+    this.author_contact = author_contact;
+
+    //can be default set to author.user.email
+    this.author_email = author_email;
+
+    //when user make a order, this will be send to customer with the confirm email.
+    this.email_append = email_append;
+
 }
 
-//about order
-this.email_append = email_append; //when user make a order, this will be send to customer with the confirm email.
+module.exports = Project_funding;
 
-//after published
-this.update_info = []; //should be [time: ,content:]
+Project_funding.prototype.save = function (maincb) {
+    // var date = new Date();
+    // var time = {
+    //     date: date,
+    //
+    //     year: date.getFullYear(),
+    //     month: (date.getMonth() + 1),
+    //     day: date.getDate(),
+    //
+    //     print_day: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
+    //     print_minute: date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " +
+    //     date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()),
+    // };
+    var project_funding = {
 
-//admin
-this.is_verified = false;;
-this.is_deleted = false;
-this.is_good = false;
+        //Project_funding
+        title: this.title,
+        description: this.description,
+        author_id: this.author_id, //the _id of user
 
-//auto
-this.author_id = author_id; //the _id of user
-this.visit_count = 0; //default 0
-this.backers_count = 0; //default 0
-this.backers = []; //should be user._id
-this.current_amount = 0;
-this.comments_count = 0;
-this.create_at = date;;
-this.update_times_count = 0;
-this.last_updated_at = date;
+        feature_image: this.feature_image, //url
+        short_blurb: this.short_blurb,
+        categroy: this.category,
+        funding_goal: this.funding_goal,
+        funding_duration: this.funding_duration,
+        reward: this.reward, //should be [{amount: ,title: ,info: , limit: ,},..]
+        video: this.video, //url
+        risk_challenges: this.risk_challenges,
 
-var date = new Date();
-  var time = {
-      date: date,
-      year : date.getFullYear(),
-      
-month : (date.getMonth() + 1),
-day : date.getDate(),      
-print_day : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate(),
-      print_minute : date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + " " + 
-      date.getHours() + ":" + (date.getMinutes() < 10 ? '0' + date.getMinutes() : date.getMinutes()), 
- }
+        author_name: this.author_name,
+        author_photo: this.author_photo,
+        author_link: this.author_link, //should be [], include weibo, wechat, etc. can be parsed by url into icons
+        author_bio: this.author_bio,//infomations
+        author_location: this.author_location,
 
+        author_contact: this.author_contact,
+        author_email: this.author_email,
+        email_append: this.email_append,
 
+        //----others----
+        //admin
+        is_verified: false,
+        is_deleted: false,
+        is_good: false,
 
+        //after published
+        update_info: [], //should be [time: ,content:]
 
+        //auto
+        pv: 0, //default 0
+
+        backers_count: 0, //default 0
+        backers: [], //should be user._id
+
+        current_amount: 0,
+
+        comments_count: 0,
+        comments: [],
+
+        create_at: new Date(),
+        update_times_count: 0,
+        last_updated_at: new Date(),
+
+    };
+    /**
+     * 使用async重写.原来的代码实在不具可读性.
+     */
+    async.waterfall([
+        function (cb) {
+            MongoClient.connect(url, function (err, db) {
+                cb(err, db);
+            });
+        },
+        function (db,cb) {
+            db.collection('project_funding').insertOne(project_funding, function (err, result) {
+                console.log("[OK]Inserted a [project_funding] into the project_funding.");
+                cb(err, db, result.ops);
+            });
+        }
+    ],function (err,db, doc) {
+        db.close();
+        maincb(err,doc);
+    })
+    
+    // var insertProjectFunding = function (db, callback) {
+    //     db.collection('project_funding').insertOne(project_funding, function (err, result) {
+    //         console.log("[OK]Inserted a [project_funding] into the project_funding.");
+    //         callback(err, result.ops);
+    //     });
+    // };
+    // //执行
+    // MongoClient.connect(url, function (err, db) {
+    //     assert.equal(null, err);
+    //     insertProjectFunding(db, function (err, user) {
+    //         db.close();
+    //         maincb(err, user);
+    //     });
+    // });
+
+}
+
+Project_funding.getAllBasicInfo = function (callback) {
+
+}
+Project_funding.getAll = function (callback) {
+
+}
+Project_funding.get = function (para, cb) {
+    // var findProjecFunding = function (db,callback) {
+    //     var cursor = db.collection('project_funding').fund(para);
+    //
+    // }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var crypto = require('crypto');
-var User = require('../models/user.js');
+var ObjectID = require('mongodb').ObjectID;
+
+var User = require('../models/users.js');
+var Project_funding = require('../models/project_funding.js')
 module.exports = function(app){
 	
 	app.get('/', function(req, res, next) {
@@ -34,7 +37,7 @@ module.exports = function(app){
 				req.flash('error','用户名已存在！');
 				return res.redirect('/register');
 			}
-			User.getbyKey_Value({email:newUser.email}, function(err,user){
+			User.get({email:newUser.email}, function(err,user){
 				if(err){
 					req.flash('error',err);
 					return res.redirect('/register');
@@ -48,7 +51,10 @@ module.exports = function(app){
 						req.flash('error',err);
 						return res.redirect('/register');
 					}
-					req.session.user = newUser;
+                    console.dir(user);
+
+                    req.session.user = user;
+                    
 					return res.redirect('/');
 				});
 			});
@@ -93,8 +99,44 @@ module.exports = function(app){
 	app.get('/start',function(req,res){
 		res.render('start',renderSession('创建您的项目 - 众客',req));
 	});
+	
+	app.post('/start',checkLogin);
+    app.post('/start',function (req,res) {
+        var currentUser = req.session.user;
+        var project_funding = new Project_funding(
+            req.body.title,
+            req.body.description,
+            currentUser._id,
+            req.body.feature_image,
+            req.body.short_blurb,
+            req.body.project_category,
+            req.body.funding_goal,
+            req.body.funding_duration,
+            req.body.reward,
+            req.body.video,
+            req.body.risk_challenges,
+            req.body.author_name,
+            req.body.author_photo,
+            req.body.author_link,
+            req.body.author_bio,
+            req.body.author_location,
+            req.body.author_contact,
+            req.body.author_email,
+            req.body.email_append);
+        project_funding.save(function (err) {
+            if(err){
+                req.flash('error',err);
+                return res.redirect('back');
+            }
+            req.flash('success','已提交审核!');
+            res.redirect('/project-funding-preview');
 
+        })
+    })
 
+    app.get('/project-funding-preview',function (req,res) {
+        res.redirect('/');
+    })
 
 
 
